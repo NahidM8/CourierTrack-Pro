a78d9205508465f36c7f243156629980fbb295dc
Nahid <Nahid@DESKTOP-VBSJ103>
2026-03-13 16:48:54 +0400

    feat: added generic repository

--- CourierTrack/src/CourierTrack.API/Program.cs
+++ CourierTrack/src/CourierTrack.API/Program.cs
@@ -1,6 +1,12 @@
+using CourierTrack.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CourierTrackDbContext>(options =>
+    options.UseSqlite(connectionString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ CourierTrack/src/CourierTrack.Infrastructure/Repositories/Implementations/Repository.cs
@@ -0,0 +1,51 @@
+﻿using CourierTrack.Infrastructure.Data.Context;
+using CourierTrack.Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourierTrack.Infrastructure.Repositories.Implementations;
+
+internal class Repository<T>(CourierTrackDbContext context) : IRepository<T> where T : class
+{
+    protected readonly CourierTrackDbContext _context = context;
+    protected readonly DbSet<T> _dbSet = context.Set<T>();
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        _dbSet.Update(entity);
+        await _context.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<bool> DeleteAsync(Guid id)
+    {
+        var entity = await GetByIdAsync(id);
+        if (entity == null)
+            return false;
+
+        _dbSet.Remove(entity);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> ExistsAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id) != null;
+    }
+}
