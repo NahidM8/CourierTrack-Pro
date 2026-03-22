b39415be168ac51dbb896c410c83d6247ec73f6a
Nahid <Nahid@DESKTOP-VBSJ103>
2026-03-22 21:02:38 +0400

    feat: added IdentityUser, Identity Dbcontext

--- CourierTrack/src/CourierTrack.Domain/Entities/User.cs
+++ CourierTrack/src/CourierTrack.Domain/Entities/User.cs
@@ -1,16 +1,14 @@
 using CourierTrack.Domain.Enums;
+using Microsoft.AspNetCore.Identity;
 
 namespace CourierTrack.Domain.Entities;
 
-public class User
+public class User : IdentityUser<Guid>
 {
-    public Guid Id { get; set; }
-    public string Email { get; set; } = string.Empty;
     public string FullName { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
     public Role Role { get; set; }
-    public DateTime CreatedAt { get; set; }
-    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public bool IsActive { get; set; } = true;
 
     public Courier? Courier { get; set; }
 }
