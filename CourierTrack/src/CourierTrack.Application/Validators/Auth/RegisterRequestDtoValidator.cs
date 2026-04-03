3a6abe74750c75f9a9b2f6e67cb1de8d726d31a9
Nahid <Nahid@DESKTOP-VBSJ103>
2026-04-03 18:57:32 +0400

    ref: updated register dto validator

--- CourierTrack/src/CourierTrack.Application/Validators/Auth/RegisterRequestDtoValidator.cs
+++ CourierTrack/src/CourierTrack.Application/Validators/Auth/RegisterRequestDtoValidator.cs
@@ -7,23 +7,17 @@ public class RegisterRequestDtoValidator : AbstractValidator<RegisterRequestDto>
 {
     public RegisterRequestDtoValidator()
     {
-        RuleFor(x => x.FullName)
-            .NotEmpty()
-            .MaximumLength(100);
-
         RuleFor(x => x.Email)
-            .NotEmpty()
-            .EmailAddress();
+            .NotEmpty().WithMessage("Email is required")
+            .EmailAddress().WithMessage("Invalid email format")
+            .MaximumLength(100).WithMessage("Email must not exceed 100 characters");
 
         RuleFor(x => x.Password)
-            .NotEmpty()
-            .MinimumLength(6);
-
-        RuleFor(x => x.PhoneNumber)
-            .NotEmpty()
-            .MaximumLength(20);
-
-        RuleFor(x => x.Role)
-            .IsInEnum();
+            .NotEmpty().WithMessage("Password is required")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters")
+            .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+            .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+            .Matches(@"[0-9]").WithMessage("Password must contain at least one number")
+            .Matches(@"[@$!%*?&#]").WithMessage("Password must contain at least one special character (@$!%*?&#)");
     }
 }
