c9d88a0e1679e16e05109217a481992a629eae4b
Nahid <Nahid@DESKTOP-VBSJ103>
2026-04-12 23:07:40 +0400

    feat: added JwtService

---
+++ CourierTrack/src/CourierTrack.Application/Interfaces/IJwtService.cs
@@ -0,0 +1,7 @@
+﻿namespace CourierTrack.Application.Interfaces;
+
+public interface IJwtService
+{
+    string GenerateAccessToken(User user, IList<string> roles);
+    RefreshToken GenerateRefreshToken(Guid userId);
+}
