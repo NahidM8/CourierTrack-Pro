d53632f725747288a153463ae28425628fd9f5f1
Nahid <Nahid@DESKTOP-VBSJ103>
2026-04-12 23:24:03 +0400

    feat: added Refresh token repository

--- CourierTrack/src/CourierTrack.Infrastructure/DependencyInjection.cs
+++ CourierTrack/src/CourierTrack.Infrastructure/DependencyInjection.cs
@@ -1,8 +1,9 @@
 using CourierTrack.Application.Interfaces;
+using CourierTrack.Domain.Entities;
 using CourierTrack.Infrastructure.Data.Context;
 using CourierTrack.Infrastructure.Repositories.Implementations;
 using CourierTrack.Infrastructure.Repositories.Interfaces;
-using CourierTrack.Domain.Entities;
+using CourierTrack.Infrastructure.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -31,6 +32,8 @@ public static class DependencyInjection
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<ICourierRepository, CourierRepository>();
         services.AddScoped<IOrderRepository, OrderRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IJwtService, JwtService>();
 
         return services;
     }
---
+++ CourierTrack/src/CourierTrack.Infrastructure/Repositories/Implementations/RefreshTokenRepository.cs
@@ -0,0 +1,47 @@
+using CourierTrack.Application.Interfaces;
+using CourierTrack.Domain.Entities;
+using CourierTrack.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourierTrack.Infrastructure.Repositories.Implementations;
+
+public class RefreshTokenRepository : IRefreshTokenRepository
+{
+    private readonly CourierTrackDbContext _context;
+
+    public RefreshTokenRepository(CourierTrackDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(string token)
+    {
+        return await _context.RefreshTokens
+            .Include(rt => rt.User)
+            .FirstOrDefaultAsync(rt => rt.Token == token);
+    }
+
+    public async Task AddAsync(RefreshToken refreshToken)
+    {
+        await _context.RefreshTokens.AddAsync(refreshToken);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(RefreshToken refreshToken)
+    {
+        _context.RefreshTokens.Update(refreshToken);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task RevokeAllUserTokensAsync(Guid userId)
+    {
+        var tokens = await _context.RefreshTokens
+            .Where(rt => rt.UserId == userId && !rt.IsRevoked)
+            .ToListAsync();
+
+        foreach (var token in tokens)
+            token.IsRevoked = true;
+
+        await _context.SaveChangesAsync();
+    }
+}
